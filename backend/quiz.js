e5b89b8dc739a28f74f9033b5e8c1d9a6ffae86d
taaro <demal2525@gmail.com>
2025-07-08 13:18:32 +0600

    added API

---
+++ backend/quiz.js
@@ -0,0 +1,188 @@
+module.exports = [
+  {
+    "Culture": {
+      "name": "Culture",
+      "description": "Bhutan has a rich and unique culture deeply rooted in Buddhism. Follow values based on harmony, respect, and spiritual wellbeing.The country places a strong emphasis on Gross National Happiness, blending modern development with cultural and environmental preservation.",
+      "quiz": [
+        {
+          "id":1,
+          "question": "What is the traditional male attire in Bhutan?",
+          "options": ["Gho", "Kira", "Lungta", "Hanbok"],
+          "answer": "Gho"
+        },
+        {
+          "id":2,
+          "question": "What type of dance is performed during Tshechu festivals",
+          "options": ["Mask Dance", "Bhangra", "Salsa", "Sword Dance"],
+          "answer": "Mask Dance"
+        },
+        {
+          "id":3,
+          "question": "What is the purpose of hoisting prayer flags",
+          "options": ["To spread blessings and good wishes", "Decoration", "Farming rituals", "Trade"],
+          "answer": "To spread blessings and good wishes"
+        },
+        {
+          "id":4,
+          "question": "What color kabney (scarf) does the King wear?",
+          "options": ["Yellow", "Orange", "White", "Red"],
+          "answer": "Yellow"
+        },
+        {
+          "id":5,
+          "question": "What is the term for Bhutanese traditional wooden architecture?",
+          "options": ["Dzong", "Pagoda", "Technology", "Trade"],
+          "answer": "Dzong"
+        },
+      ]
+    },
+    "Histrory": {
+      "name": "Bhutan History",
+      "description": " ",
+      "quiz": [
+        {
+          "id":1,
+          "question": "Who was the first hereditary King of Bhutan?",
+          "options": ["Jigme Dorji Wangchuck", "Ugyen Wangchuck", "Zhabdrung Ngawang Namgyel", "Jigme Singye Wangchuck"],
+          "answer": "Ugyen Wangchuck"
+        },
+        {
+          "id":2,
+          "question": "In which year did Bhutan become a constitutional monarchy?",
+          "options": ["2001", "1998", "2008", "2010"],
+          "answer": "2008"
+        },
+        {
+          "id":3,
+          "question": "Which figure is credited with unifying Bhutan in the 17th century?",
+          "options": ["Zhabdrung Ngawang Namgyel", "Guru Rinpoche", "Jigme Namgyel", "Pema Lingpa"],
+          "answer": "Zhabdrung Ngawang Namgyel"
+        },
+        {
+          "id":4,
+          "question": "When did Bhutan join the United Nations?",
+          "options": ["1981", "1965", "1985", "1971"],
+          "answer": "1971"
+        },
+        {
+          "id":5,
+          "question": "What was Bhutan's traditional form of government before monarchy?",
+          "options": ["Theocratic", "Democratic", " Colonial", "Federal"],
+          "answer": "Theocratic"
+        }
+      ]
+    },
+    "Geography": {
+      "name": "Bhutan Geography",
+      "description": " ",
+      "quiz": [
+        {
+          "id":1,
+          "question": "What is the capital city of Bhutan?",
+          "options": ["Paro", "Thimphu", "Punakha", "Gelephu"],
+          "answer": "Thimphu"
+        },
+        {
+          "id":2,
+          "question": "Which mountain is the highest in Bhutan?",
+          "options": ["Gangkhar Puensum", "Masang Gang", "Gangkhar Puensum", "Kangchenjunga"],
+          "answer": "Gangkhar Puensum"
+        },
+        {
+          "id":3,
+          "question": "Bhutan is landlocked between which two countries?",
+          "options": ["India and China", "Nepal and India", "Bangladesh and China", " India and Myanmar"],
+          "answer": "India and China"
+        },
+        {
+          "id":4,
+          "question": "Which river flows through Punakha Dzong?",
+          "options": ["Mo Chhu", "Wang Chhu,", "Drangme Chhu","Pho Chhu",],
+          "answer": "Pho Chu"
+        },
+        {
+          "id":5,
+          "question": "How many districts (Dzongkhags) are there in Bhutan?",
+          "options": ["19", "20", "25", "30"],
+          "answer": "20"
+        }
+      ]
+    },
+    "Religion and Spirituality": {
+      "name": "Bhutan Geography",
+      "description": " ",
+      "quiz": [
+        {
+          "id":1,
+          "question": "What is the dominant religion in Bhutan?",
+          "options": ["Buddhism", "Hinduism", "Islam", "Christianity"],
+          "answer": "Thimphu"
+        },
+        {
+          "id":2,
+          "question": "Who is considered the Second Buddha in Bhutanese tradition?",
+          "options": ["Guru Rinpoche", "Buddha Shakyamuni", "Dalai Lama", "Zhabdrung"],
+          "answer": "Gangkhar Puensum"
+        },
+        {
+          "id":3,
+          "question": "What is a chorten?",
+          "options": ["A stupa or religious monument", "Fortress", "Temple bell", "Mask"],
+          "answer": "A stupa or religious monument"
+        },
+        {
+          "id":4,
+          "question": "Which sect of Buddhism is practiced in Bhutan?",
+          "options": ["Drukpa Kagyu", "Theravada", "Zen","Mahayana"],
+          "answer": "Drukpa Kagyu"
+        },
+        {
+          "id":5,
+          "question": "What do prayer wheels symbolize?",
+          "options": ["Cycle of karma and spiritual purification", "Time passing", "Seasons", "Festivals"],
+          "answer": "Cycle of karma and spiritual purification"
+        }
+      ]
+    },
+
+    "Gross National Happiness": {
+      "name": "Gross National Happiness",
+      "description": " ",
+      "quiz": [
+        {
+          "id":1,
+          "question": "Which Bhutanese King introduced the concept of GNH?",
+          "options": ["King Jigme Singye Wangchuck", "King Jigme Dorji Wangchuck", "King Ugyen Wangchuck", "King Jigme Khesar Namgyel"],
+          "answer": "King Jigme Singye Wangchuck"
+        },
+        {
+          "id":2,
+          "question": "Which of these is NOT a pillar of GNH?",
+          "options": ["Economic Prosperity","Good Governance", "Cultural Preservation", "Environmental Conservation]"],
+          "answer": "Economic Prosperity"
+        },
+        {
+          "id":3,
+          "question": "In which year was the GNH index formally introduced?",
+          "options": ["2008", "2005", "1999", "2011"],
+          "answer": "2008"
+        },
+        {
+          "id":4,
+          "question": "GNH focuses more on ___ than economic wealth",
+          "options": ["Wellbeing", "Industry", "Technology", "Trade"],
+          "answer": "Drukpa Kagyu"
+        },
+        {
+          "id":5,
+          "question": "What does GNH prioritize over economic growth?",
+          "options": ["Trade","Wellbeing","Industry","Profit"],
+          "answer": "Wellbeing"
+        }
+      ]
+    }
+
+  
+
+  }
+]
