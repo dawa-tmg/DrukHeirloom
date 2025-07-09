753afbd92eba5cd5389f7221dc174b7c92b9f609
Phuntsho <pwangmo2019@gmail.com>
2025-07-09 11:54:41 +0600

    Adds multiple dzongs and informations about it

--- backend/heritage.js
+++ backend/heritage.js
@@ -1,17 +1,44 @@
 module.exports = [
   {
-    name: "Something about Heritage",
+    name: "Punakha Dzong",
     image: "../../assets/Dzong.jpg",
-    description: "Bhutan is a small, landlocked kingdom nestled in the eastern Himalayas between India and China. Known for its breathtaking landscapes, rich culture, and deep spiritual heritage, Bhutan is often called the 'Land of the Thunder Dragon.'"
+    description: "Punakha Dzong was the administrative center and the seat of the Government of Bhutan until 1955 when the capital was moved to Thimphu.Punakha Dzong was built by Zhabdrung Ngawang Namgyal for several key reasons: to commemorate a victory over the Tibetan forces, to serve as an administrative and religious center, to house sacred Buddhist relics, and to symbolize national unity. It was also strategically located at the confluence of the Pho Chhu and Mo Chhu rivers, which was believed to represent the harmonious union of masculine and feminine forces. ",
   },
   {
-    name: "Something about Heritage",
+    name: "Kyichu Lhakhang",
     image: "../../assets/lhakang.jpg",
-    description: "Bhutan is a small, landlocked kingdom nestled in the eastern Himalayas between India and China. Known for its breathtaking landscapes, rich culture, and deep spiritual heritage, Bhutan is often called the 'Land of the Thunder Dragon.'"
+    description: "Kyichu Lhakhang, one of Bhutan's oldest and most revered temples, is steeped in history and legend. It is believed to have been built in the 7th century by the Tibetan King Songtsen Gampo, as part of a network of 108 temples aimed at subduing a powerful demoness hindering the spread of Buddhism.",
   },
   {
-    name: "Something about Heritage",
+    name: "Gangtey Gonpa",
     image: "../../assets/monastry.jpeg",
-    description: "Bhutan is a small, landlocked kingdom nestled in the eastern Himalayas between India and China. Known for its breathtaking landscapes, rich culture, and deep spiritual heritage, Bhutan is often called the 'Land of the Thunder Dragon.'"
-  }
+    description: "Gangtey Goenpa was established in 1613 by Pema Lingpa's grandson on a spur in the Black Mountains called the Gangteng Sang Nga Chöling, which translates to “summit for the teaching of the dharma.” Local timber was used for the temple's beams, doors, pillars, and windows.Bhutan is a small, landlocked kingdom nestled in the eastern Himalayas between India and China. Known for its breathtaking landscapes, rich culture, and deep spiritual heritage, Bhutan is often called the 'Land of the Thunder Dragon.'"
+  },
+  {
+    name:"Trongsa Dzong",
+    image: "../../assets/Trongsa-Dzong.jpg",
+    description:"Trongsa Dzong was initially built by Chhogyel Mingyur Tenpa, who was sent by Zhabdrung Ngawang Namgyal to bring eastern Bhutan under central control. The dzong was later enlarged by Desi Tenzin Rabgyeat towards the end of the 17th century.Trongsa Dzong, located in central Bhutan, has a rich history as a strategic fortress and a center of power. It was initially established in 1543 by Yongzin Ngagi Wangchuk as a monastery called Mondrupley, and later became the Trongsa Dzong in 1647 under Zhabdrung Ngawang Namgyal. The dzong played a vital role in Bhutan's unification and governance, with all Bhutanese kings historically serving as Trongsa Penlop (governor) before ascending to the throne.", 
+  },
+  {
+    name:"Trashi chhoe Dzong",
+    image:"../../assets/Trashi cho dzong.jpg",
+    description:"This splendid dzong, north of the city on the west bank of the Wang Chhu, dominates the valley, looking out over a cascade of terraced fields. It's Thimphu's grandest building by far, and served as the official seat of the Druk Desi, the head of the secular government that shared power with the religious authorities, from the 18th to the 19th centuries. The dzong was the site of the lavish formal coronation of the fifth king in 2008.The building you see today is actually not the original Thimphu dzong. The first fortress – the Dho-Ngen Dzong (Blue Stone Dzong) – was erected in 1216 on the hillside where Dechen Phodrang now stands, and it was adopted as the seat of Lama Phajo Drukgom Shigpo, who brought the Drukpa Kagyu lineage to Bhutan. The fort was renamed Trashi Chho Dzong (Fortress of the Glorious Religion) when it passed from the descendants of Lama Phajo to Zhabdrung Ngawang Namgyal in 1641.",
+  },
+  {
+    name:"Wangdue Phodrang Dzong",
+    image:"../../assets/Wangdue Phodrang.jpg",
+    description:"Wangdue Phodrang Dzong, also known as Wangdi, is a historic fortress located in central Bhutan, founded in 1638 by Zhabdrung Ngawang Namgyal. It served as a crucial administrative and religious center and was strategically chosen for its commanding view of the valleys below. The dzong is known for its role in unifying western, central, and southern Bhutan and protecting against southern invasions. It has faced significant damage from fires and earthquakes throughout its history, including a major fire in 2012 that destroyed much of the structure. Currently, it is undergoing reconstruction using traditional methods and materials.  ",
+
+  },
+  {
+    name:"Trashigang Dzong",
+    image:"../../assets/Trashigang Dzong.jpg",
+    description:"Trashigang is a town in eastern Bhutan and the district capital of the Trashigang Dzongkhag. The town lies to the east side of the valley above the Drangme Chhu river just south of where it is joined by the Gamri River.",
+
+  },
+  {
+   name:"National Animal",
+   image:"../../assets/Takin.jpg",
+    description:"The national animal of Bhutan is the Takin (Budorcas taxicolor). It's a unique, goat-antelope-like animal, known for its distinctive appearance and its association with Bhutanese mythology and religious history.", 
+  },
 ];
