3947962a36b7855b02107790d56c356a3408a83a
dawa.t <dawadorji295@gmail.com>
2025-07-09 09:00:26 +0600

    Adds Heritage API and Implementation

---
+++ backend/heritage.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: "Something about Heritage",
+    image: "../../assets/Dzong.jpg",
+    description: "Bhutan is a small, landlocked kingdom nestled in the eastern Himalayas between India and China. Known for its breathtaking landscapes, rich culture, and deep spiritual heritage, Bhutan is often called the 'Land of the Thunder Dragon.'"
+  },
+  {
+    name: "Something about Heritage",
+    image: "../../assets/Dzong.jpg",
+    description: "Bhutan is a small, landlocked kingdom nestled in the eastern Himalayas between India and China. Known for its breathtaking landscapes, rich culture, and deep spiritual heritage, Bhutan is often called the 'Land of the Thunder Dragon.'"
+  },
+  {
+    name: "Something about Heritage",
+    image: "../../assets/monastry.jpeg",
+    description: "Bhutan is a small, landlocked kingdom nestled in the eastern Himalayas between India and China. Known for its breathtaking landscapes, rich culture, and deep spiritual heritage, Bhutan is often called the 'Land of the Thunder Dragon.'"
+  }
+];
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require('cors');
-const quizz = require("./quiz");
+const quiz = require("./quiz");
+const heritage = require("./heritage");
 const app = express();
 const port = 3001;
 
@@ -10,7 +11,11 @@ app.use(express.json());
 
 //READ
 app.get("/quiz", (req, res) => {
-    res.send(quizz);
+    res.send(quiz);
+});
+
+app.get("/heritage", (req, res) => {
+    res.send(heritage);
 });
 
 
